9880f1975b55d5c14f5738c5b8c7eb4bd1999416
pande-rupesh <rupeshpande2001@gmail.com>
2025-08-04 16:47:52 +0530

    Updated DataBase Connnection

--- src/main/java/com/DB/DBconnect.java
+++ src/main/java/com/DB/DBconnect.java
@@ -13,7 +13,7 @@ public class DBconnect {
 	  try {
 		
 		  Class.forName("com.mysql.cj.jdbc.Driver");
-		  connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ebook", "root", "pande9096");
+		  connection=DriverManager.getConnection("url", "username", "password");
 	} catch (ClassNotFoundException e) {
 	e.printStackTrace();
 	}
